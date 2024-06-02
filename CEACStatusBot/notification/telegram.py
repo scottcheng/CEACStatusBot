fe3758ed67a65f16abcc785e8eef3534d80992c7
Scott Cheng <1612169+scottcheng@users.noreply.github.com>
2024-06-02 22:35:25 +0100

    remove telegram message prefix

--- CEACStatusBot/notification/telegram.py
+++ CEACStatusBot/notification/telegram.py
@@ -13,7 +13,7 @@ class TelegramNotificationHandle(NotificationHandle):
     def send(self, result):
         # {'success': True, 'visa_type': 'NONIMMIGRANT VISA APPLICATION', 'status': 'Issued', 'case_created': '30-Aug-2022', 'case_last_updated': '19-Oct-2022', 'description': 'Your visa is in final processing. If you have not received it in more than 10 working days, please see the webpage for contact information of the embassy or consulate where you submitted your application.', 'application_num': '***'}
 
-        message_title = f"[CEACStatusBot] {result['application_num_origin']}: {result['status']}"
+        message_title = f"{result['application_num_origin']}: {result['status']}"
         message_content = html.escape(json.dumps(result, indent=2))
 
         # Construct the message text with the title in bold
